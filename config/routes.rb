a6494b20f58532a5a788a31c7533c145c95e402d
Dexter1210 <deveshsharma1210@gmail.com>
2020-09-24 14:28:24 +0530

    Implementation of login field using bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  resources :sessions,only: [:new,:create,:destroy]
   resources :users
   root 'home#index'
   get 'home/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'signup', to:'users#new',as:'signup'
+  get 'login', to:'sessions#new', as:'login'
+  get 'logout', to:'sessions#destroy',as:'logout'  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
